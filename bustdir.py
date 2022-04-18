80a0cf35a1729fe6816e683714db8114934bd89b
LogicalAlmond <logicalalmond@protonmail.com>
2022-04-17 21:51:47 -0400

    Added directory scanning

--- bustdir.py
+++ bustdir.py
@@ -1,11 +1,12 @@
-import socket
 import requests
-import json
 import argparse
-import os.path
-from datetime import datetime
 
-VERSION = "0.0.1"
+RED = '\u001b[31m'
+GREEN = '\u001b[32m'
+YELLOW = '\u001b[33m'
+BLUE = '\u001b[34m'
+RESET = '\u001b[0m'
+VERSION = "0.1.0"
 BANNER = '''
  ________  ___  ___  ________  _________  ________  ___  ________     
 |\   __  \|\  \|\  \|\   ____\|\___   ___ \   ___ \|\  \|\   __  \    
@@ -16,3 +17,32 @@ BANNER = '''
     \|_______|\|_______|\_________\   \|__|  \|_______|\|__|\|__|\|__|
                        \|_________|                                   
 '''
+print(BANNER)
+print(f'[*] BustDir Version: {VERSION}')
+
+
+def dirscan(domain):
+    with open('Wordlists/directories.txt', 'r') as wordlist:
+        for entry in wordlist:
+            strippedEntry = entry.strip('\n')
+            url = f'https://{domain}/{strippedEntry}'
+            try:
+                req = requests.get(url, allow_redirects=False)
+                res = req.status_code
+                if res == 200:
+                    print(f'{GREEN}[+] {url}{RESET}')
+                else:
+                    print(f'{RED}[-] {url}{RESET}')
+            except Exception as e:
+                print(f'Error in request.')
+                
+                
+if __name__ == "__main__":
+    
+    p = argparse.ArgumentParser()
+    p.add_argument('-url', required=True, help='Url to scan')
+    
+    args = p.parse_args()
+    url = args.url
+    
+    dirscan(url)
